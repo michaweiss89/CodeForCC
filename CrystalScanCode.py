a3170c4d2d3ad8267144440579d19ee2c1b92ad4
Physics <michaweiss89@gmail.com>
2018-11-17 20:06:38 +0200

    Update CrystalScanCode.py

--- CrystalScanCode.py
+++ CrystalScanCode.py
@@ -55,11 +55,7 @@ def first_state(wl): #sorry...
     return spf ,lpf
 
 def second_state(wl): #sorry...
-    if  (wl<400):
-        SPF.command('pos=6')
-        print("SPF is in posiotion 6 Empty")
-        spf = np.nan
-    if (wl>=400)&(wl<440):
+    if  (wl<440):
         SPF.command('pos=1')
         print("SPF is in posiotion 1 Wavlength 450")
         spf = 450
