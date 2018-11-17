0f267800e04fad750d74114f043dbf02dda010fa
Physics <michaweiss89@gmail.com>
2018-11-17 19:14:04 +0200

    updated code I do not remmember

--- CrystalScanCode.py
+++ CrystalScanCode.py
@@ -2,7 +2,7 @@ def first_state(wl): #sorry...
     if  (wl<400):
         SPF.command('pos=6')
         print("SPF is in posiotion 6 Empty")
-        spf = 0
+        spf = np.nan
     if (wl>=400)&(wl<440):
         SPF.command('pos=1')
         print("SPF is in posiotion 1 Wavlength 450")
@@ -26,12 +26,12 @@ def first_state(wl): #sorry...
     if (wl>=640):
         SPF.command('pos=6')
         print("SPF is in posiotion 6 Empty")
-        spf = 0
+        spf = np.nan
         
     if  (wl<350):
         LPF.command('pos=6')
         print("LPF is in posiotion 6 Empty")
-        lpf = 0
+        lpf = np.nan
     if (wl>=350)&(wl<400):
         LPF.command('pos=1')
         print("LPF is in posiotion 1 Wavlength  400")
@@ -55,14 +55,14 @@ def first_state(wl): #sorry...
     if (wl>=600):
         LPF.command('pos=6')
         print("LPF is in posiotion 6 Empty") 
-        lpf = 0
+        lpf = np.nan
     return spf ,lpf
 
 def second_state(wl): #sorry...
     if  (wl<400):
         SPF.command('pos=6')
         print("SPF is in posiotion 6 Empty")
-        spf = 0
+        spf = np.nan
     if (wl>=400)&(wl<440):
         SPF.command('pos=1')
         print("SPF is in posiotion 1 Wavlength 450")
@@ -86,7 +86,7 @@ def second_state(wl): #sorry...
     if (wl>=640):
         SPF.command('pos=6')
         print("SPF is in posiotion 6 Empty")
-        spf = 0
+        spf = np.nan
     LPF.command('pos=2')
     print("LPF is in posiotion 2 Wavlength 450")
     lpf = 450
@@ -99,24 +99,25 @@ def set_spectro_grating(num):
         ser7.write(b'SetGrating\r')
         ser7.write(b'1\r')
         time.sleep(0.1)
-        while(ser7.read(150)!=b'spectro grating set to 1\r\n' and j< 30):
+        while(ser7.read(150)!=b'1\r\n' and j< 90):
             time.sleep(1)
             j = j + 1
-        if j==30:
+        if j==90:
             print("error: Andor timed out")
             return 0
         print("spectro grating 1 is set", f" - took {j} iteration with 1 s sleep")
+        return 0
     if num == 2:
         ser7.write(b'SetGrating\r')
         ser7.write(b'2\r')
         time.sleep(0.1)
-        while(ser7.read(150)!=b'spectro grating set to 2\r\n' and j< 30):
+        while(ser7.read(150)!=b'2\r\n' and j< 90):
             time.sleep(0.5)
             j = j+1
-    if j==30:
+    if j==90:
         print("error: Andor timed out")
         return 0
-    print("spectro grating 2 is set", f" -  took {j} iteration with 1 s sleep")
+        print("spectro grating 2 is set", f" -  took {j} iteration with 1 s sleep")
     return 0
 
 def set_up(wl,state):
@@ -152,13 +153,29 @@ def set_exposure_time(s):
     j = 1
     ser7.write(b'SetExposureTime\r')
     ser7.write(str(s).encode() + b'\r')
-    while(ser7.read(150)!=b'Exposure time set to '+str(s).encode()+b'\r\n' and j< 30):
+    while(ser7.read(150)!=str(s).encode()+b'\r\n' and j< 30):
         time.sleep(1)
         j = j + 1
     if j==30:
         print("error: Andor timed out")
         return 0
     print(f'Exposure time set to {s} seconds', f" - took {j} iteration with 1 s sleep")
+    return 0
+
+def setwavlength(wl):
+    print(f"{str(datetime.datetime.now())} : Setting spectro center wavelngth")
+    ser7.read(150)
+    ser7.write(b'SetWavelength\r')
+    ser7.write(str(wl).encode()+b'\r')
+    A = ser7.read(150)
+    j=0
+    while(A==b""and j<30):
+        time.sleep(0.5)
+        j = j + 1
+        A = ser7.read(150)
+    if j>30:
+        print("Andor time out no wavelength set")
+    print("Andor:",A, f" - took {j} iteration with 0.5 s sleep")
     return 0
 
 def save_ascii_file(ID, wl, crystal, exposuretime ,measurment_number,date,t,bg):
@@ -194,6 +211,7 @@ def take_measurement(ID, wl, crystal, exposuretime ,measurment_number):
         power.append(pm.measure_power(wl))
     print(f'power: {np.mean(power)}, std:{np.std(power)}, # samples: {len(power)}')
     save_ascii_file(ID, wl, crystal, exposuretime ,measurment_number,date,t,"bg")
+
     ser7.write(b'SetShutter\ropen\r') 
     print("--taking sig")
     print(ser7.read(150))
@@ -220,7 +238,7 @@ def measurment(crystal ,measurment_number, ID, wl):
     exposure_t = [10]
     j=0
     click = 1 
-    while (j < 10):
+    while (j < 4):
         j=j+1
         print("")
         print("----------taking measurment")
@@ -236,6 +254,20 @@ def measurment(crystal ,measurment_number, ID, wl):
         num.append(C)
         t.append(D)
         exposure_t.append(1)
+    
+    measurment_number = measurment_number +1
+    print("")
+    print("----------taking measurment")
+    measurment_numbert = "{0:0=3d}".format(measurment_number)   
+    print(f'mesurment number {measurment_numbert}')    
+    set_exposure_time(0.1)
+    A,B,C,D= take_measurement(ID, wl, crystal, 0.1 ,measurment_numbert)
+    power.append(A)
+    std.append(B)
+    num.append(C)
+    t.append(D)
+    exposure_t.append(0.1)
+
     t.extend(exposure_t)
     num.extend(t)
     std.extend(num)
@@ -249,13 +281,13 @@ def create_data_fram(data, crystal , ID, date):
     columns=["# mesurment","ex_wl","spectro_center_wl",'grating','spf','lpf','lpf2',"av_power","std_power","num_power",'time_taken',"ex_time","em_wl","counts"]
     col = len(columns)-2
     click = 1
-    n = int(len(data)/(11*col))
-    data = np.array(data).reshape(col*n,11)
+    n = int(len(data)/(6*col))
+    data = np.array(data).reshape(col*n,6)
     data_neat = []
     for i in range(col):
         temp = data[i::col,:]
         data_neat = np.append(data_neat,temp)
-    data_neat = data_neat.reshape(col,11*n).T
+    data_neat = data_neat.reshape(col,6*n).T
     path =r"C:\Users\Physics\Documents\CrystalScans\\"+crystal+r'\\ID_'+ID+r"\Rawdata\\"
     file_list = os.listdir(path)
     M = []
@@ -271,7 +303,7 @@ def create_data_fram(data, crystal , ID, date):
             continue
         M = np.hstack((M,d.T))
     print(n)
-    Result = pd.DataFrame(M.reshape(1600*11*n,col+2), columns=["# mesurment","ex_wl","spectro_center_wl",'grating','spf','lpf','lpf2',"av_power",
+    Result = pd.DataFrame(M.reshape(1600*6*n,col+2), columns=["# mesurment","ex_wl","spectro_center_wl",'grating','spf','lpf','lpf2',"av_power",
                                                           "std_power","num_power",'time_taken',"ex_time","em_wl","counts"])
     Result.to_csv(r"C:\Users\Physics\Documents\CrystalScans\\"+crystal+r'\ID_'+ID+r"\\Scan.csv")              
     return Result
@@ -312,22 +344,6 @@ def creatfiles(crystal,ID):
     return 0 
 
 
-def setwavlength(wl):
-    print(f"{str(datetime.datetime.now())} : Setting spectro center wavelngth")
-    ser7.read(150)
-    ser7.write(b'SetWavelength\r')
-    ser7.write(str(wl).encode()+b'\r')
-    A = ser7.read(150)
-    j=0
-    while(A==b""and j<30):
-        time.sleep(0.5)
-        j = j + 1
-        A = ser7.read(150)
-    if j>30:
-        print("Andor time out no wavelength set")
-    print("Andor:",A, f" - took {j} iteration with 0.5 s sleep")
-    return 0
-
 def crystal_scan(crystal):
     now = datetime.datetime.now()
     date = str(now.day)+str(now.month)+str(now.year-2000)
@@ -344,11 +360,11 @@ def crystal_scan(crystal):
         setGrating(ser1, 2) 
         SPF.command('pos=6')# remmember to uncomment
         print("SPF is in posiotion 6 Empty")
-        spf = 0
+        spf = np.nan
         LPF.command('pos=6')# remmember to uncomment
         print("LPF is in posiotion 6 Empty")
-        lpf = 0
-        lpf2 = 0
+        lpf = np.nan
+        lpf2 = np.nan
         motor.write(down_position)
         print("355 nm LPF before sample")
         click1 = 1
@@ -372,21 +388,22 @@ def crystal_scan(crystal):
             goto(ser1,wl)
             click3 = 1
             for state in set_up_list:
-                data.extend([measurment_number + number + 1 for number in range(11)])
+                data.extend([measurment_number + number + 1 for number in range(6)])
                 print("---------------------Set Up state: ", state)
                 spf,lpf,spectro_grating,spectro_center_wl = set_up(wl,state)
-                data.extend([wl for number in range(11)])
-                data.extend([spectro_center_wl for number in range(11)])
-                data.extend([spectro_grating for number in range(11)])
-                data.extend([spf for number in range(11)])
-                data.extend([lpf for number in range(11)])
-                data.extend([lpf2 for number in range(11)])
+                data.extend([wl for number in range(6)])
+                data.extend([spectro_center_wl for number in range(6)])
+                data.extend([spectro_grating for number in range(6)])
+                data.extend([spf for number in range(6)])
+                data.extend([lpf for number in range(6)])
+                data.extend([lpf2 for number in range(6)])
                 power,measurment_number = measurment(c ,measurment_number, ID, wl)
                 data.extend(power) 
                 with open(r"C:\Users\Physics\Documents\CrystalScans\\"+c+r"\\ID_"+ID+r"\back_up.txt", "wb") as fp:
-                    pickle.dump(data, fp)           
+                    pickle.dump(data, fp)
+                ser7.write(b'ClearScreen\r')
         time.sleep(2)
         print(data)
         print(c, ID, date, t)
 #        df = create_data_fram(data, c, ID, date)
-    return df
+    return 0
